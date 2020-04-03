f113ea71670d1d2b4751fa8825c10d1c6045e43d
shifu204 <472879445@qq.com>
2020-04-03 21:12:06 +0800

    逆向工程代码生成

--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,32 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+	@Autowired
+	ProductService productService;
+
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		Product byId = productService.getById(1);
+        System.out.println(byId.getName());
+
+        log.debug("34343");
 	}
 
 }
---
+++ gmall-sms/src/main/java/com/atguigu/com/sms/mapper/FlashPromotionMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.com.sms.mapper;
+
+import com.atguigu.gmall.sms.entity.FlashPromotion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 限时购表 Mapper 接口
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-04-03
+ */
+public interface FlashPromotionMapper extends BaseMapper<FlashPromotion> {
+
+}
