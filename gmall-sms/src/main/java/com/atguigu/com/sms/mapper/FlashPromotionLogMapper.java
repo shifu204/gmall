54a7393a4d45e3fc988818baacfff882c7767ca7
shifu204 <472879445@qq.com>
2020-04-03 21:13:25 +0800

    逆向工程代码生成

---
+++ gmall-sms/src/main/java/com/atguigu/com/sms/mapper/FlashPromotionLogMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.com.sms.mapper;
+
+import com.atguigu.gmall.sms.entity.FlashPromotionLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 限时购通知记录 Mapper 接口
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-04-03
+ */
+public interface FlashPromotionLogMapper extends BaseMapper<FlashPromotionLog> {
+
+}
---
+++ gmall-sms/src/main/java/com/atguigu/com/sms/mapper/FlashPromotionProductRelationMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.com.sms.mapper;
+
+import com.atguigu.gmall.sms.entity.FlashPromotionProductRelation;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 商品限时购与商品关系表 Mapper 接口
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-04-03
+ */
+public interface FlashPromotionProductRelationMapper extends BaseMapper<FlashPromotionProductRelation> {
+
+}
